8abe29235531539f4e913d9238ff0ce5dcda2884
ihsan Ç <a.ihsan.celik96@gmail.com>
2023-06-09 17:02:13 +0300

    File Logger Service

---
+++ Services/FileLogger.cs
@@ -0,0 +1,38 @@
+﻿namespace rtlog.Services
+{
+    public class FileLogger : ILogService
+    {
+        public void Log(string application, string version, string type, string message, string? stackTrace)
+        {
+            var filePath = GetFilePath(application, version);
+            var log = GetLogMessage(message, stackTrace, type);
+            File.AppendAllText(filePath, log);
+        }
+
+        private string GetLogMessage(string message, string? stackTrace, string type)
+        {
+            var time = DateTime.Now.ToString("HH:mm:ss");
+            string log = string.Empty;
+
+            log += "-START-----------------------------------\n";
+            log += $"message:{message}\n";
+            log += $"stackTrace:{stackTrace}\n";
+            log += $"type:{type}\n";
+            log += $"time:{time}\n";
+            log += "-END------------------------------------\n";
+
+            return log;
+        }
+
+        private string GetFilePath(string application, string version)
+        {
+            var currentDateStr = DateTime.Now.ToString("yyyy-MM-dd");
+            string filePath = $"logs/{application}-{version}-{currentDateStr}.log";
+
+            if (!File.Exists(filePath))
+                File.Create(filePath);
+
+            return filePath;
+        }
+    }
+}
---
+++ Services/ILogService.cs
@@ -0,0 +1,7 @@
+﻿namespace rtlog.Services
+{
+    public interface ILogService
+    {
+        void Log(string application, string version, string type, string message, string? stackTrace);
+    }
+}
